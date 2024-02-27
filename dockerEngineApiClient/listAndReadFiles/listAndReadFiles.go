32c3e1c6d4e3b0d9365ef3670263e9d4d2b40428
Rajesh Kumar <srk280392@gmail.com>
2024-02-27 19:09:42 +0530

    added listing and reading files

---
+++ dockerEngineApiClient/listAndReadFiles/listAndReadFiles.go
@@ -0,0 +1,57 @@
+package listAndReadFiles
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func ReadContentsOfFile(apiClient *client.Client, ctx context.Context, containerIDOrName string, filePath string) (string, error) {
+
+	// Read the contents of filepath
+	readContent, _, err := apiClient.CopyFromContainer(ctx, containerIDOrName, filePath)
+	if err != nil {
+		return "", err
+	}
+	defer readContent.Close()
+	content, err := io.ReadAll(readContent)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func ListFilesAndDirectories(apiClient *client.Client, ctx context.Context, containerIDOrName string, path string) ([]string, error) {
+
+	// ecec into the container and run ls -l to get the list of files and directories
+	execId, err := apiClient.ContainerExecCreate(ctx, containerIDOrName, types.ExecConfig{
+		Cmd:          []string{"ls", "-l", path},
+		AttachStderr: true,
+		AttachStdout: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp, err := apiClient.ContainerExecAttach(ctx, execId.ID, types.ExecStartCheck{})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Close()
+	scanner := bufio.NewScanner(resp.Reader)
+	var files []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line) // splits a string into substrings based on white space (spaces, tabs, or newlines)
+		if len(fields) > 8 {
+			files = append(files, fields[8]) // field 8 contains the file name eg drwxr-xr-x 1 root root    4096 Sep 20 16:43 nginx
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return files, nil
+}
--- dockerEngineApiClient/main.go
+++ dockerEngineApiClient/main.go
@@ -10,23 +10,25 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/rk280392/dockerEngineApiClient/checkLogs"
 	"github.com/rk280392/dockerEngineApiClient/inspectContainers"
+	"github.com/rk280392/dockerEngineApiClient/listAndReadFiles"
 	"github.com/rk280392/dockerEngineApiClient/resourceMonitor"
 )
 
 func main() {
 
-	command := flag.String("command", "", "Command to execute: 'inspect', 'stats', or 'logs'")
+	command := flag.String("command", "", "Command to execute: 'inspect', 'stats', 'logs', 'list', 'read'")
 	containerIDOrName := flag.String("container", "", "Container ID or name")
 	follow := flag.Bool("follow", false, "Follow logs in real-time")
 	timeStamp := flag.String("timestamp", "", "Show logs since timestamp (e.g. 2013-01-02T13:23:37Z) or relative (e.g. 42m for 42 minutes)")
 	interval := flag.Duration("interval", 5*time.Second, "Interval for resource monitoring")
 	loglevel := flag.String("loglevel", "info", "loglevel log filtering (e.g., 'info', 'error)")
+	readFilePath := flag.String("readfilepath", "", "read the contents of the file")
 
 	// Parse command-line arguments
 	flag.Parse()
 
 	if *command == "" || *containerIDOrName == "" {
-		fmt.Println("Usage: ./main -command <command> -container <container-id-or-name> [-follow] [-timestamp <timestamp>] [-interval <interval>] [-loglevel <loglevel>]")
+		fmt.Println("Usage: ./main -command <command> -container <container-id-or-name> [-follow] [-timestamp <timestamp>] [-interval <interval>] [-loglevel <loglevel>] [-readFilePath <filepath>]")
 		os.Exit(1)
 	}
 	ctx := context.Background()
@@ -54,8 +56,22 @@ func main() {
 			fmt.Println("Failed to follow container logs:", err)
 			os.Exit(1)
 		}
+	case "list":
+		output, err := listAndReadFiles.ListFilesAndDirectories(apiClient, ctx, *containerIDOrName, "/etc")
+		if err != nil {
+			fmt.Println("Failed tp hey list of files", err)
+		}
+		fmt.Println(output)
+	case "read":
+		output, err := listAndReadFiles.ReadContentsOfFile(apiClient, ctx, *containerIDOrName, *readFilePath)
+		if err != nil {
+			fmt.Println("Failed to read contents of the file")
+		}
+		fmt.Println(output)
+	default:
+		fmt.Println("Usage: ./program <command>")
+		fmt.Println("Available commands: inspect, stats, logs, list, read")
 	}
-
 }
 
 func initAPIClient() *client.Client {
